8b48e1904706efdbf91144e818a41c217c54d20d
buan <andre.bussmann@nis.ch>
2023-09-04 13:15:58 +0200

    add log statements

--- lib/binaries/chrome_xml.ts
+++ lib/binaries/chrome_xml.ts
@@ -9,82 +9,92 @@ import {BinaryUrl} from './binary';
 import {XmlConfigSource} from './config_source';
 
 export class ChromeXml extends XmlConfigSource {
-  maxVersion = Config.binaryVersions().maxChrome;
+    maxVersion = Config.binaryVersions().maxChrome;
 
-  constructor() {
-    super('chrome', Config.cdnUrls()['chrome']);
-  }
-
-  getUrl(version: string): Promise<BinaryUrl> {
-    if (version === 'latest') {
-      return this.getLatestChromeDriverVersion();
-    } else {
-      return this.getSpecificChromeDriverVersion(version);
+    constructor() {
+        super('chrome', Config.cdnUrls()['chrome']);
     }
-  }
 
-  /**
-   * Get a list of chrome drivers paths available for the configuration OS type and architecture.
-   */
-  getVersionList(): Promise<string[]> {
-    return this.getXml().then(xml => {
-      let versionPaths: string[] = [];
-      let osType = this.getOsTypeName();
-
-      for (let content of xml.ListBucketResult.Contents) {
-        let contentKey: string = content.Key[0];
-
-        if (
-            // Filter for 32-bit devices, make sure x64 is not an option
-            (this.osarch.includes('64') || !contentKey.includes('64')) &&
-            // Filter for x86 macs, make sure m1 is not an option
-            ((this.ostype === 'Darwin' && this.osarch === 'arm64') || !contentKey.includes('m1'))) {
-          // Filter for only the osType
-          if (contentKey.includes(osType)) {
-            versionPaths.push(contentKey);
-          }
+    getUrl(version: string): Promise<BinaryUrl> {
+        if (version === 'latest') {
+            return this.getLatestChromeDriverVersion();
+        } else {
+            return this.getSpecificChromeDriverVersion(version);
         }
-      }
-      return versionPaths;
-    });
-  }
-
-  /**
-   * Helper method, gets the ostype and gets the name used by the XML
-   */
-  getOsTypeName(): string {
-    // Get the os type name.
-    if (this.ostype === 'Darwin') {
-      return 'mac';
-    } else if (this.ostype === 'Windows_NT') {
-      return 'win';
-    } else {
-      return 'linux';
     }
-  }
+
+    /**
+     * Get a list of chrome drivers paths available for the configuration OS type and architecture.
+     */
+    getVersionList(): Promise<string[]> {
+        return this.getXml().then(xml => {
+            let versionPaths: string[] = [];
+            let osType = this.getOsTypeName();
+
+            for (let content of xml.ListBucketResult.Contents) {
+                let contentKey: string = content.Key[0];
+
+                if (
+                    // Filter for 32-bit devices, make sure x64 is not an option
+                    (this.osarch.includes('64') || !contentKey.includes('64')) &&
+                    // Filter for x86 macs, make sure m1 is not an option
+                    ((this.ostype === 'Darwin' && this.osarch === 'arm64') || !contentKey.includes('m1'))) {
+                    // Filter for only the osType
+                    if (contentKey.includes(osType)) {
+                        versionPaths.push(contentKey);
+                    }
+                }
+            }
+            return versionPaths;
+        });
+    }
+
+    /**
+     * Helper method, gets the ostype and gets the name used by the XML
+     */
+    getOsTypeName(): string {
+        // Get the os type name.
+        if (this.ostype === 'Darwin') {
+            return 'mac';
+        } else if (this.ostype === 'Windows_NT') {
+            return 'win';
+        } else {
+            return 'linux';
+        }
+    }
 
     /**
      * Gets the latest item from the XML.
      */
     private getLatestChromeDriverVersion(): Promise<BinaryUrl> {
+        console.log("getLatestChromeDriverVersion");
         const latestReleaseUrl: string =
             'https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions-with-downloads.json';
+        console.log("getLatestChromeDriverVersion url: ", latestReleaseUrl);
         return requestBody(latestReleaseUrl).then(body => {
             const latestVersion_Body = JSON.parse(body)['channels']['Stable']
+            console.log("getLatestChromeDriverVersion latestVersion_Body: ", latestVersion_Body);
             const latestVersion = latestVersion_Body['version'];
+            console.log("getLatestChromeDriverVersion latestVersion: ", latestVersion);
             const latestVersion_Url = latestVersion_Body['downloads']['chromedriver'].find(
-                    (obj: any) => obj['platform'] == 'win64')['url'];
+                (obj: any) => obj['platform'] == 'win64')['url'];
+            console.log("getLatestChromeDriverVersion latestVersion_Url: ", latestVersion_Url);
 
             const latestMajorVersion: string = latestVersion.split('.')[0];
+            console.log("getLatestChromeDriverVersion latestMajorVersion: ", latestMajorVersion);
 
             const localVersion_FileName: string =
                 fs.readdirSync(path.resolve(__dirname, '..', '..', '..', 'selenium'))
                     .find(f => f.startsWith(`chromedriver_${latestMajorVersion}`)) || '';
+            console.log("getLatestChromeDriverVersion localVersion_FileName: ", localVersion_FileName);
 
             const localVersion: string = localVersion_FileName.slice(13, -4);
+            console.log("getLatestChromeDriverVersion localVersion: ", localVersion);
             const localVersion_Url: string = latestVersion_Url.replace(latestVersion, localVersion);
+            console.log("getLatestChromeDriverVersion localVersion_Url: ", localVersion_Url);
 
             const localMajorVersion: string = localVersion.split('.')[0];
+            console.log("getLatestChromeDriverVersion localMajorVersion: ", localMajorVersion);
 
             if (latestMajorVersion == localMajorVersion) {
                 return Promise.resolve({
@@ -100,58 +110,58 @@ export class ChromeXml extends XmlConfigSource {
         });
     }
 
-  /**
-   * Gets a specific item from the XML.
-   */
-  private getSpecificChromeDriverVersion(inputVersion: string): Promise<BinaryUrl> {
-    return this.getVersionList().then(list => {
-      const specificVersion = getValidSemver(inputVersion);
-      if (specificVersion === '') {
-        throw new Error(`version ${inputVersion} ChromeDriver does not exist`)
-      }
-      let itemFound = '';
-      for (let item of list) {
-        // Get a semantic version.
-        let version = item.split('/')[0];
-        if (semver.valid(version) == null) {
-          const lookUpVersion = getValidSemver(version);
-
-          if (semver.valid(lookUpVersion)) {
-            // Check to see if the specified version matches.
-            if (lookUpVersion === specificVersion) {
-              // When item found is null, check the os arch
-              // 64-bit version works OR not 64-bit version and the path does not have '64'
-              if (itemFound == '') {
-                if (this.osarch === 'x64' ||
-                    (this.osarch !== 'x64' && !item.includes(this.getOsTypeName() + '64'))) {
-                  itemFound = item;
-                }
-                if (this.osarch === 'arm64' && this.ostype === 'Darwin' && item.includes('m1')) {
-                  itemFound = item;
-                }
-              }
-              // If the semantic version is the same, check os arch.
-              // For 64-bit systems, prefer the 64-bit version.
-              else if (this.osarch === 'x64') {
-                // No win64 version exists, so even on x64 we need to look for win32
-                const osTypeNameAndArch =
-                    this.getOsTypeName() + (this.getOsTypeName() === 'win' ? '32' : '64');
-
-                if (item.includes(osTypeNameAndArch)) {
-                  itemFound = item;
-                }
-              }
+    /**
+     * Gets a specific item from the XML.
+     */
+    private getSpecificChromeDriverVersion(inputVersion: string): Promise<BinaryUrl> {
+        return this.getVersionList().then(list => {
+            const specificVersion = getValidSemver(inputVersion);
+            if (specificVersion === '') {
+                throw new Error(`version ${inputVersion} ChromeDriver does not exist`)
             }
-          }
-        }
-      }
-      if (itemFound == '') {
-        return {url: '', version: inputVersion};
-      } else {
-        return {url: Config.cdnUrls().chrome + itemFound, version: inputVersion};
-      }
-    });
-  }
+            let itemFound = '';
+            for (let item of list) {
+                // Get a semantic version.
+                let version = item.split('/')[0];
+                if (semver.valid(version) == null) {
+                    const lookUpVersion = getValidSemver(version);
+
+                    if (semver.valid(lookUpVersion)) {
+                        // Check to see if the specified version matches.
+                        if (lookUpVersion === specificVersion) {
+                            // When item found is null, check the os arch
+                            // 64-bit version works OR not 64-bit version and the path does not have '64'
+                            if (itemFound == '') {
+                                if (this.osarch === 'x64' ||
+                                    (this.osarch !== 'x64' && !item.includes(this.getOsTypeName() + '64'))) {
+                                    itemFound = item;
+                                }
+                                if (this.osarch === 'arm64' && this.ostype === 'Darwin' && item.includes('m1')) {
+                                    itemFound = item;
+                                }
+                            }
+                                // If the semantic version is the same, check os arch.
+                            // For 64-bit systems, prefer the 64-bit version.
+                            else if (this.osarch === 'x64') {
+                                // No win64 version exists, so even on x64 we need to look for win32
+                                const osTypeNameAndArch =
+                                    this.getOsTypeName() + (this.getOsTypeName() === 'win' ? '32' : '64');
+
+                                if (item.includes(osTypeNameAndArch)) {
+                                    itemFound = item;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if (itemFound == '') {
+                return { url: '', version: inputVersion };
+            } else {
+                return { url: Config.cdnUrls().chrome + itemFound, version: inputVersion };
+            }
+        });
+    }
 }
 
 /**
@@ -167,26 +177,26 @@ export class ChromeXml extends XmlConfigSource {
  * @param version
  */
 export function getValidSemver(version: string): string {
-  let lookUpVersion = '';
-  // This supports downloading 2.46
-  try {
-    const oldRegex = /(\d+.\d+)/g;
-    const exec = oldRegex.exec(version);
-    if (exec) {
-      lookUpVersion = exec[1] + '.0';
+    let lookUpVersion = '';
+    // This supports downloading 2.46
+    try {
+        const oldRegex = /(\d+.\d+)/g;
+        const exec = oldRegex.exec(version);
+        if (exec) {
+            lookUpVersion = exec[1] + '.0';
+        }
+    } catch (_) {
+        // no-op: is this is not valid, do not throw here.
     }
-  } catch (_) {
-    // no-op: is this is not valid, do not throw here.
-  }
-  // This supports downloading 74.0.3729.6
-  try {
-    const newRegex = /(\d+.\d+.\d+).\d+/g;
-    const exec = newRegex.exec(version);
-    if (exec) {
-      lookUpVersion = exec[1];
+    // This supports downloading 74.0.3729.6
+    try {
+        const newRegex = /(\d+.\d+.\d+).\d+/g;
+        const exec = newRegex.exec(version);
+        if (exec) {
+            lookUpVersion = exec[1];
+        }
+    } catch (_) {
+        // no-op: if this does not work, use the other regex pattern.
     }
-  } catch (_) {
-    // no-op: if this does not work, use the other regex pattern.
-  }
-  return lookUpVersion;
+    return lookUpVersion;
 }
